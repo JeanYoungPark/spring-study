67bcccbcc16882c273658a811f3612d539aad700
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-03-31 00:32:16 +0900

    순수 JDBC 연결해보기

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -1,32 +1,29 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
-import hello.hellospring.repository.MemberReository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import hello.hellospring.repository.MemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberReository memberReository;
+    private final MemberRepository memberRepository;
 
-    public MemberService(MemberReository memberReository) {
-        this.memberReository = memberReository;
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     //회원가입
     public Long join(Member member){
         //같은 이름이 있는 중복 회원X
         validateDuplicateMember(member); //중복 회원 검증
-        memberReository.save(member);
+        memberRepository.save(member);
         return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
-        memberReository.findByName(member.getName())
+        memberRepository.findByName(member.getName())
                 .ifPresent(m -> { //값이 있으면, optional로 감싸져 있을때 가능
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
@@ -34,10 +31,10 @@ public class MemberService {
 
     //전체 회원 조회
     public List<Member> findMembers(){
-        return memberReository.findAll();
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId){
-        return memberReository.findById(memberId);
+        return memberRepository.findById(memberId);
     }
 }
