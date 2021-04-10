a0ed50688c9f0ac0ad177a96b3d11991e79ef29a
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2021-04-11 03:55:34 +0900

    branch test

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+//test
 @Controller
 public class HelloController {
     
